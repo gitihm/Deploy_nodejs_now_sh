990e689a8e770294d244aeb7dcf90e4d663dd331
gitihm <hama89700@gmail.com>
2020-06-08 22:11:46 +0700

    test

--- index.js
+++ index.js
@@ -6,4 +6,13 @@ app.get("/", (req, res) => {
   res.json({message : "OK"})
 });
 
+app.post("/", (req, res) => {
+  if(req.body.name === "air"){
+    res.json({status : true})
+  }else{
+    res.json({status : false})
+  }
+  
+});
+
 const server = app.listen(port);
